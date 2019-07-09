1d5a0cd909a01102c1345c18fcdc1b5fc0dd76d4
Georgy Timoshenskiy <timoshenskij.gd@edu.spbstu.ru>
2019-07-09 19:59:22 +0300

    manual window added

---
+++ src/main/java/UI/ControllerManual.java
@@ -0,0 +1,47 @@
+package UI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ControllerManual {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button generateButton;
+
+    @FXML
+    private Button helpButton;
+
+    @FXML
+    private AnchorPane previewField;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    void showManual(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
+
+
