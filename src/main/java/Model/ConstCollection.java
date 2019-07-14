52634523547d6081cbc1d8dde27ce0c4d61b54a6
SatykovED <47986860+SatyukovED@users.noreply.github.com>
2019-07-14 15:14:22 +0300

    update consts

--- src/main/java/Model/ConstCollection.java
+++ src/main/java/Model/ConstCollection.java
@@ -3,11 +3,23 @@ package Model;
 public class ConstCollection {
     public static final int IMAGE_WIDTH        = 150;
     public static final int IMAGE_HEIGHT       = 100;
-    public static final int MIN_OBJECT_WIDTH   = 50;
-    public static final int MIN_OBJECT_HEIGHT  = 20;
+
+    public static final int MIN_OBJECT_WIDTH   = 70;
     public static final int MAX_OBJECT_WIDTH   = 130;
-    public static final int MAX_OBJECT_HEIGHT  = 30;
-    public static final String PATH            = "D://Generator/";
+    public static final int MIN_OBJECT_HEIGHT  = 30;
+    public static final int MAX_OBJECT_HEIGHT  = 40;
+
+    public static final String PATH            = "C://Generator/";
+
+    public static final int MIN_NOISE_WIDTH    = 60;
+    public static final int MAX_NOISE_WIDTH    = 90;
+    public static final int MIN_NOISE_HEIGHT   = 30;
+    public static final int MAX_NOISE_HEIGHT   = 60;
+
+    /* Next stage of development - realization of multithreading
+    public static final int THREAD_COUNT     = 0;
+    */
+
     /* Next stage of development - recognition of different controls included in one form
     public static final int FORM_WIDTH         = 0;
     public static final int FORM_HEIGHT        = 0;
