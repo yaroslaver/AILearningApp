9bd877bf80aeab21523abff5d461447becf75ebd
SatykovED <47986860+SatyukovED@users.noreply.github.com>
2019-07-16 15:55:51 +0300

    change const threadcount

--- src/main/java/Model/ConstCollection.java
+++ src/main/java/Model/ConstCollection.java
@@ -16,9 +16,7 @@ public class ConstCollection {
     public static final int MIN_NOISE_HEIGHT   = 30;
     public static final int MAX_NOISE_HEIGHT   = 60;
 
-    /* Next stage of development - realization of multithreading
-    public static final int THREAD_COUNT     = 0;
-    */
+    public static final int THREAD_COUNT     = 6;
 
     /* Next stage of development - recognition of different controls included in one form
     public static final int FORM_WIDTH         = 0;
